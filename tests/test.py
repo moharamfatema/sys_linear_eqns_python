46a0c7e7e9e9d7de8bdb20e3ed2e7090394003ea
Fatema Moharam <89610248+moharamfatema@users.noreply.github.com>
2022-01-05 00:11:41 +0200

    + test gauss siedel

--- tests/test.py
+++ tests/test.py
@@ -81,3 +81,41 @@ class TestMethods(unittest.TestCase):
         x = methods.gauss_elimination(number_of_equations=5,input_equations=a)
         self.assertEqual(x.shape,correct.shape)
         self.assertAlmostEqual(x.all(),correct.all(),5)
+
+    def testGaussSiedel(self):
+        a = numpy.array([[2, 1, 4], [1, 2, 3], [4, -1 ,2]])
+        b = numpy.array([1,1.5,2])
+        correct = numpy.array([-1,1,-0.5])
+        x = methods.gauss_seidel(a,b)
+        self.assertEqual(x.shape,correct.shape)
+        self.assertAlmostEqual(x.all(),correct.all(),5)
+
+        a = numpy.array([[4,6,2,-2],[2,0,5,-3],[-4,-3,-5,4],[8,18,-2,3]])
+        b = numpy.array([8,4,1,40])
+        correct = numpy.array([9,-6,3,2])
+        x = methods.gauss_seidel(a,b)
+        self.assertEqual(x.shape,correct.shape)
+        self.assertAlmostEqual(x.all(),correct.all(),5)
+
+        a = numpy.array([[1,2,5,-1,7],[0,-8,5,0,10],[0,-4,5,6,1],[0,6,25,99,2],[0,0,33,2,15]])
+        b = numpy.array([1,5,3,4,2])
+        correct = numpy.array([1.939052136,-0.5869276124,1737/31207.0,3863/62414.0,82/31207.0])
+        x = methods.gauss_seidel(a,b)
+        self.assertEqual(x.shape,correct.shape)
+        self.assertAlmostEqual(x.all(),correct.all(),5)
+
+        temp = a[0]
+        a[0] = a[4]
+        a[4] = temp
+
+        temp = correct[0]
+        correct[0] = correct[4]
+        correct[4] = temp
+
+        temp = b[0]
+        b[0] = b[4]
+        b[4] = temp
+
+        x = methods.gauss_seidel(a,b)
+        self.assertEqual(x.shape,correct.shape)
+        self.assertAlmostEqual(x.all(),correct.all(),5)
